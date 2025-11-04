4de4153cc2d9c25a8bdc9b066eed0a369ab336f7
martin <martin_charrier@yahoo.fr>
2025-11-04 20:54:40 +0100

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+
 import "./globals.css";
 
 const geistSans = Geist({
