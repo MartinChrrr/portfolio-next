3f83eaca308767a2ce78014811f601e9935ea8d3
martin <martin_charrier@yahoo.fr>
2025-11-05 13:48:01 +0100

    fixed header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,17 +1,32 @@
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { Section } from "./Section"
 import { GithubIcon } from "./icons/GiithubIcon"
+import { LinkedinIcon } from "./icons/LinkedinIcon";
+import Link from "next/link";
+
 
 export const Header = () => {
     return (
-        <header className="sticky top-0">
-            <Section classname="flex items-baseline">
-                <h1 className="text-4xl font-bold text-primary-foreground">Martin Charrier</h1>
+        <header className="sticky top-0 py-4">
+            <Section classname="flex items-baseline justify-between">
+                <h1 className="text-2xl font-bold text-primary">Martin Charrier</h1>
                 <div className="flex-1">
-                    <ul>
-                          <Button className="size-6">
-                            <GithubIcon size={16} className="text-foreground"></GithubIcon>
-                          </Button>
+                    <ul className="flex justify-end">
+                        <li>
+                            <Link className={`${buttonVariants({ variant: "outline" ,size:"icon"})} p-0 mx-1`} 
+                        
+                                href="https://github.com/MartinChrrr">
+                                <GithubIcon size={12} className="text-foreground" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className={`${buttonVariants({ variant: "outline", size:"icon" })}  p-0 mx-1`}    
+                                href="https://github.com/MartinChrrr">
+                                <LinkedinIcon size={12} className="text-foreground"/>
+                                
+                            </Link>
+                        </li>
+         
                     </ul>
                 </div>
                 
