aa849651cd97e43551964b3faefcce0812554d7e
martin <martin_charrier@yahoo.fr>
2025-11-06 18:35:54 +0100

    presentation photo to change

---
+++ app/_components/Presentation.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithoutRef } from "react"
+import { Section } from "./Section"
+const Code = ({className, ...props}: ComponentPropsWithoutRef<"span">) => {
+    return <span className={`bg-accent/30 hover:bg-accent/50 border border-accent rounded-sm text-primary p-1  ,${className}`} {...props}/>
+
+}
+
+export const Presentation = () => {
+return (
+    <Section classname="flex max-lg:flex-col items-start gap-4">
+        <div className="flex-[3] w-full flex flex-col gap-2">
+            <h2 className="font-caption text-4xl text-primary font-bold">Martin Charrier</h2>
+            <h3 className="font-caption text-3xl">Développeur</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit reiciendis omnis praesentium nulla maiores cupiditate, atque,
+                 dolorum quaerat nam nemo fugit saepe sequi vel necessitatibus ad voluptatibus, ducimus libero doloribus.</p>
+        </div>
+        <div className="flex-[2]">
+            <img src="https://media.licdn.com/dms/image/v2/D4E03AQFzz4PMJGZfIw/profile-displayphoto-scale_100_100/B4EZk3_.WBKYAc-/0/1757581142080?e=1764201600&v=beta&t=XraN_N6zvHHOGfH2IwrX8IyUYFcQnhQLGOiaQ3Uxmp4" 
+            className="w-full h-auto max-w-sm" 
+             alt="photo de Martin Charrier"  />
+        </div>
+
+    </Section>
+)
+}
