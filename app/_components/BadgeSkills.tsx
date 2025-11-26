dcf7837cfec1bd786547f796f9dabf92deb4fd3b
martin <martin_charrier@yahoo.fr>
2025-11-26 08:05:13 +0100

    Skills

--- app/_components/BadgeSkills.tsx
+++ app/_components/BadgeSkills.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react"
 
-const BadgeSkills = (props: PropsWithChildren<{classname?: string}>) =>{
+export const BadgeSkills = (props: PropsWithChildren<{classname?: string}>) =>{
     return (
         <div className={`inline-flex items-center rounded-full border 
         px-2.5 py-0.5 text-xs font-semibold focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 
--- app/_components/Skills.tsx
+++ app/_components/Skills.tsx
@@ -3,38 +3,72 @@ import { Section } from "./Section"
 import { CodeIcons } from "./icons/CodeIcons"
 import { SpeechIcon } from "./icons/SpeechIcon"
 import { SoftSkillsIcon } from "./icons/SoftSkillsIcon"
+import { Card } from "@/components/ui/card"
+import { BadgeSkills } from "./BadgeSkills"
 
 export const Skills = () => {
+    const languages: string[] = [
+        "PHP", 
+        "Java", 
+        "Typescript", 
+        "Python", 
+        "C++", 
+        "C#"
+    ];
+    const frameworks: string[] = [
+        "Spring", 
+        "Vue", 
+        "Unity", 
+        "Tailwind", 
+        "Django", 
+        "Node", 
+        "Express"
+    ]
+    const softSkills: string[] = [
+        "Rigueur", 
+        "Autonomie",
+        "Gout du challenge",
+        "Travail en équipe"
+    ]
+    const tools: string[] = [
+        "Git", 
+        "Github",
+        "Figma",
+        "Linux"
+    ]
+
     return (
         <Section classname="flex w-full flex-col items-start gap-4 ">
             <h2 className="pb-2 text-3xl font-semibold tracking-tight first:mt-0">What I'm good at </h2>
             <div className="flex max-lg:flex-col w-full">
-                <div className="flex flex-col flex-[1] gap-4 items-center">
-                    <CodeIcons size={48}/>
-                    <h3 className="text-2xl font-semibold tracking-tight">Languages</h3>
-                    <p className="text-muted-foreground text-sm">Java,C#</p>
-                </div>
-                <div className="flex flex-col flex-[1] gap-4">
-                    <SpeechIcon size={48}/>
-                    <h3 className="text-2xl font-semibold tracking-tight">Languages</h3>
-                    <div className="flex flex-col gap-2">
-                        <p className="text-muted-foreground text-sm">English</p>
-                        <p className="text-muted-foreground text-sm">French</p>
-
+                <Card className="p-6 flex-1 flex flex-col"> 
+                    <h3>Langages</h3>
+                    <div className="flex flex-wrap gap-2">
+                       {languages.map((language, index) => (<BadgeSkills key = {index}>{language}</BadgeSkills>))}
                     </div>
-                    
-                </div>
-                <div className="flex flex-col flex-[1]">
-                    <SoftSkillsIcon size={48}/>
-                    <h3 className="text-2xl font-semibold tracking-tight">Languages</h3>
-                    <p className="text-muted-foreground text-sm">Java,C#</p>
-                </div>
-                <div className="flex flex-col flex-[1]">
-                    <CodeIcons size={48}/>
-                    <h3 className="text-2xl font-semibold tracking-tight">Languages</h3>
-                    <p className="text-muted-foreground text-sm">Java,C#</p>
-                </div>
 
+                </Card>
+                <Card className="p-6 flex-1 "> 
+                    <h3>Librairie et Framework</h3>
+                    <div className="flex flex-wrap gap-2">
+                       {frameworks.map((framework, index) => (<BadgeSkills key = {index}>{framework}</BadgeSkills>))}
+                    </div>
+                </Card>
+            </div>
+            <div className="flex max-lg:flex-col w-full">
+                <Card className="p-6 flex-1 flex flex-col"> 
+                    <h3>Soft Skills</h3>
+                    <div className="flex flex-wrap gap-2">
+                       {softSkills.map((skill, index) => (<BadgeSkills key = {index}>{skill}</BadgeSkills>))}
+                    </div>
+
+                </Card>
+                <Card className="p-6 flex-1 "> 
+                    <h3>Outils</h3>
+                    <div className="flex flex-wrap gap-2">
+                       {frameworks.map((framework, index) => (<BadgeSkills key = {index}>{framework}</BadgeSkills>))}
+                    </div>
+                </Card>
             </div>
             
 
