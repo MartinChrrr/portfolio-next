39531ade6171bc5ae312d7dc98b01a007e45956f
martin <martin_charrier@yahoo.fr>
2025-11-27 09:54:31 +0100

    Changement ui hover education row

--- app/_components/EducationRow.tsx
+++ app/_components/EducationRow.tsx
@@ -10,10 +10,10 @@ export type EducationRowProps = {
 
 export const EducationRow = (props: EducationRowProps) => {
     return(
-        <Link href={props.url} className="inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-2">          
+        <Link href={props.url} className="group inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-2">          
                 <img src={props.image} alt={props.title} className=" w-8 h-8 object-contain" />
             <div>
-                <div className="text-lg font-semibold">{props.title}</div>
+                <div className="text-lg font-semibold group-hover:text-primary">{props.title}</div>
                 <p className="text-muted-foreground text-sm">{props.school}</p>
             </div>
             <div className="ml-auto">
