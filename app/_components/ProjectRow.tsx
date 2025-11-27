76f09d696c9f3bc4963ef048c98003dd6c140708
martin <martin_charrier@yahoo.fr>
2025-11-27 09:51:00 +0100

    changement ui Hover

--- app/_components/ProjectRow.tsx
+++ app/_components/ProjectRow.tsx
@@ -11,12 +11,12 @@ export type ProjectRowProps = {
 
 export const ProjectRow = (props: ProjectRowProps) => {
     return(
-        <Link href={props.url} className="inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-2">
-            <span className="bg-accent text-accent-foreground p-3 rounded-sm">
+        <Link href={props.url} className="group inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-2">
+            <span className="bg-primary/20 text-accent-foreground p-3 rounded-sm group-hover:text-primary">
                 <props.logo/>
             </span>
             <div>
-                <div className="text-lg font-semibold">{props.title}</div>
+                <div className="text-lg font-semibold group-hover:text-primary">{props.title}</div>
                 <p className="text-muted-foreground text-lg">{props.description}</p>
             </div>
         </Link>
